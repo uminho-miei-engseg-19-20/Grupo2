e812f0fd8bba69434a08d3d0266b64b8f1657ddb
Rafaela Soares <36676088+cosmicgirl97@users.noreply.github.com>
2020-07-06 23:49:15 +0100

    ficheiro não utilizado

--- Projetos/Projeto3/cmd_config.rs
+++
@@ -1,9 +0,0 @@
-// ApplicationId da entidade, fornecida pela AMA
-static APPLICATION_ID: &'static str = "b826359c-06f8-425e-8ec3-50a97a418916";
-
-
-
-// Devolve APPLICATION_ID (fornecida pela AMA).
-fn get_appid() -> &'static str {
-	return APPLICATION_ID;
-}
